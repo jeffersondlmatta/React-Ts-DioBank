52a7fe3975619568d19408077d653c7a4bbe9216
Jefferson da Mata <100040285+jeffersondlmatta@users.noreply.github.com>
2024-05-09 10:28:19 -0300

    atualizado

--- src/services/login.test.tsx
+++ src/services/login.test.tsx
@@ -4,9 +4,14 @@ describe('login', () => {
 
     const mockAlert = jest.fn()
     window.alert = mockAlert
+    const mockEmail = 'jeff@jeff'
 
     it('Deve exibir um alert com boas vindas', () => {
-        login()
-        expect(mockAlert).toHaveBeenCalledWith('Bem vinda!')
+        login(mockEmail)
+        expect(mockAlert).toHaveBeenCalledWith(`Bem vindo ${mockEmail}!`)
+    })
+    it('nao deve exibir boas vindas sem o email ', () => {
+        login(mockEmail)
+        expect(mockAlert).not.toHaveBeenCalledWith('Bem vindo')
     })
 })
